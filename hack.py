83c142e3ce29676b53985d755b0186bc6e71e8e5
ankamurat <114828043+ankamurat@users.noreply.github.com>
2022-12-17 15:20:04 +0300

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,51 @@
+import time
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+print(Fore.RED,"Merhaba")
+time.sleep(3)
+print("")
+print("Bilgisayarınızın Verileri Ele Geçiriliyor...")
+time.sleep(2)
+print("")
+print(Fore.BLUE,"Kapatırsanız Bilgisayarınız Birdaha Açılmamak Üzere Kilitlenecektir")
+time.sleep(1)
+print("...")
+time.sleep(2)
+print(Fore.GREEN,"...")
+time.sleep(3)
+print("  ...")
+time.sleep(3)
+print("  ...")
+time.sleep(2)
+print(Fore.GREEN,"...")
+time.sleep(1)
+print("...")
+time.sleep(3)
+print("")
+print ("""
+
+  /$$$$$$  /$$   /$$ /$$   /$$  /$$$$$$  /$$      /$$ /$$   /$$ /$$$$$$$   /$$$$$$  /$$$$$$$$
+ /$$__  $$| $$$ | $$| $$  /$$/ /$$__  $$| $$$    /$$$| $$  | $$| $$__  $$ /$$__  $$|__  $$__/
+| $$  \ $$| $$$$| $$| $$ /$$/ | $$  \ $$| $$$$  /$$$$| $$  | $$| $$  \ $$| $$  \ $$   | $$   
+| $$$$$$$$| $$ $$ $$| $$$$$/  | $$$$$$$$| $$ $$/$$ $$| $$  | $$| $$$$$$$/| $$$$$$$$   | $$   
+| $$__  $$| $$  $$$$| $$  $$  | $$__  $$| $$  $$$| $$| $$  | $$| $$__  $$| $$__  $$   | $$   
+| $$  | $$| $$\  $$$| $$\  $$ | $$  | $$| $$\  $ | $$| $$  | $$| $$  \ $$| $$  | $$   | $$   
+| $$  | $$| $$ \  $$| $$ \  $$| $$  | $$| $$ \/  | $$|  $$$$$$/| $$  | $$| $$  | $$   | $$   
+|__/  |__/|__/  \__/|__/  \__/|__/  |__/|__/     |__/ \______/ |__/  |__/|__/  |__/   |__/   
+                                                                                             
+                                                                                             
+                                                                                             
+
+""")
+time.sleep(5)
+print("")
+print(Fore.BLUE,"Bilgisayarınızın Verileri Ele Geçirildi :)")
+time.sleep(2)
+print("")
+print(Fore.MAGENTA,"Geçmiş Olsun :)")
+
+
+
+                                                                                             
+                                                                                             
